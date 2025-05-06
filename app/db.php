05e7ce6304968091a33beb8f651c0f71bf9702a7
Hoang Phu <hoangphudang20@gmai.com>
2025-05-06 19:36:47 +0700

    update src

---
+++ app/db.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'db';
+$db = 'team_intro';
+$user = 'root';
+$pass = 'root';
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
